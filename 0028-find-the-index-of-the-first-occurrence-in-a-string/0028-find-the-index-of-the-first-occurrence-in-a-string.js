4f790df08e33127d55b813691fa7500d75e65545
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-02 22:30:39 +0530

    Time: 56 ms (40.33%), Space: 51.5 MB (5.53%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,16 @@
+var strStr = function(haystack, needle) {
+    
+
+    for(let i=0;i<=haystack.length-needle.length;i++){
+        let str=""
+       for(let j=i;j<i+needle.length;j++){
+            str+=haystack[j]
+        }
+        if(str==needle){
+            return i
+        }
+
+    }
+    return -1
+  
+};
